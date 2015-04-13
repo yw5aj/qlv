cf4e33e8d4f23136d83efa0d385d485120d81125
Yuxiang Wang <yw5aj@virginia.edu>
2015-04-13 00:46:15 -0400

    Worked more on the qlv class - Cauchy directly?

--- qlvmod.f90
+++ qlvmod.f90
@@ -12,6 +12,16 @@ contains
         integer, intent(in) :: nqlv
         real(dp), intent(inout) :: sigma(3, 3), ccj(3, 3, 3, 3),&
             siso(3, 3), svol(3, 3)
+        integer, parameter :: nhyper = (size(props) - nqlv * 2) / 2
+        real(dp), parameter :: sigma0(3, 3) = sigma
+        integer :: k1, k2, k3, k4, alpha
+        real(dp) :: h0(3, 3, nqlv), xi(nqlv), sigma1(3, 3)
+        ! Initialize then sum up for all terms
+        do alpha = 1, nqlv
+
+        end do
+
+
     end subroutine qlv
 
 end module qlvmod
